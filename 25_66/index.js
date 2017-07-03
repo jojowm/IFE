c108ee15f59f6fb71565609434031af675741541
jojowm <weimin19940508@126.com>
2017-07-03 21:51:59 +0800

    complete

--- 25_66/index.js
+++ 25_66/index.js
@@ -1,10 +1,10 @@
-window.onload = function () {
+window.onload = function() {
     setHeight();
     scrollPage();
 }
 
 // 设定sidebar的高度
-function setHeight () {
+function setHeight() {
     var outerSidebar = document.getElementById('sidebar-outer');
     var innerSidebar = document.getElementById('sidebar-inner');
     var windowHeight = window.innerHeight;
@@ -16,23 +16,25 @@ function setHeight () {
 }
 
 
-function scrollPage () {
+function scrollPage() {
     var thead = document.getElementById('table-head');
     var atd = document.getElementsByClassName('table-row')[0].getElementsByTagName('td');
     var ath = document.getElementsByClassName('head-row')[0].getElementsByTagName('th');
 
-    document.addEventListener('scroll', fixThead, false);
+    document.addEventListener('scroll', function() {
+        fixThead();
+        resetHerght()
+    }, false);
 
     // 页面滚动时，固定thead
-    function fixThead () {
+    function fixThead() {
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
         if (scrollTop >= 350) {
             thead.className = 'fix-head';
             var tdArray = Array.from(atd);
             var thArray = Array.from(ath);
 
-            for (var i = 0; i < tdArray.length; i++)  {
-                console.log(window.getComputedStyle(tdArray[i]).width)
+            for (var i = 0; i < tdArray.length; i++) {
                 thArray[i].style.width = window.getComputedStyle(tdArray[i]).width
                 tdArray[i].style.width = thArray[i].style.width + 'px';
             }
@@ -41,4 +43,26 @@ function scrollPage () {
             thead.className = '';
         }
     }
+    // 页面滚动时，计算sidebar在视口中的高度
+    function resetHerght() {
+        var outerSidebar = document.getElementById('sidebar-outer');
+        var innerSidebar = document.getElementById('sidebar-inner');
+        var sidebarList = document.getElementById('sidebar-list');
+
+        var windowHeight = window.innerHeight;
+        var sideTop = outerSidebar.getBoundingClientRect().top;
+
+        var sideHeight = windowHeight - sideTop;
+        var ulHeight = parseInt(window.getComputedStyle(sidebarList).height, 10);
+
+        if (ulHeight >= sideHeight) {
+            outerSidebar.style.height = sideHeight + 'px';
+            innerSidebar.style.height = sideHeight + 'px';
+        } else {
+            outerSidebar.style.height = ulHeight + 'px';
+            innerSidebar.style.height = ulHeight + 'px';
+        }
+
+    }
+
 }
