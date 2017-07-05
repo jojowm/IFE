4f4761e5da6ada7faf72a9164a315c9f602fee7e
jojowm <weimin19940508@126.com>
2017-07-05 22:39:11 +0800

    30/66

--- 29_66/data.js
+++ 29_66/data.js
@@ -1,73 +1,73 @@
 var data = [
     {
-        title: 'level 1 index 1',
-        children: [
+        "title": "level 1 index 1",
+        "children": [
             {
-                title: 'level 2 index 1'
+                "title": "level 2 index 1"
             },
             {
-                title: 'level 2 index 2'
+                "title": "level 2 index 2"
             }
         ]
     },
     {
-        title: 'level 1 index 2',
-        children: [
+        "title": "level 1 index 2",
+        "children": [
             {
-                title: 'level 2 index 3'
+                "title": "level 2 index 3"
             },
             {
-                title: 'level 2 index 4'
+                "title": "level 2 index 4"
+            }
+        ]
+   },
+    {
+        "title": "level 1 index 3",
+        "children": [
+            {
+                "title": "level 2 index 5"
+            },
+            {
+                "title": "level 2 index 6"
             }
         ]
     },
     {
-        title: 'level 1 index 3',
-        children: [
+        "title": "level 1 index 4",
+        "children": [
             {
-                title: 'level 2 index 5'
+                "title": "level 2 index 7"
             },
             {
-                title: 'level 2 index 6'
+                "title": "level 2 index 8"
+            },
+            {
+                "title": "level 2 index 9"
             }
         ]
     },
     {
-        title: 'level 1 index 4',
-        children: [
+        "title": "level 1 index 5",
+        "children": [
             {
-                title: 'level 2 index 7'
+                "title": "level 2 index 10"
             },
             {
-                title: 'level 2 index 8'
+                "title": "level 2 index 11"
             },
             {
-                title: 'level 2 index 9'
+                "title": "level 2 index 12"
             }
         ]
     },
     {
-        title: 'level 1 index 5',
-        children: [
+        "title": "level 1 index 6",
+        "children": [
             {
-                title: 'level 2 index 10'
+                "title": "level 2 index 13"
             },
             {
-                title: 'level 2 index 11'
-            },
-            {
-                title: 'level 2 index 12'
-            }
-        ]
-    },
-    {
-        title: 'level 1 index 6',
-        children: [
-            {
-                title: 'level 2 index 13'
-            },
-            {
-                title: 'level 2 index 14'
+                "title": "level 2 index 14"
             }
         ]
     }
